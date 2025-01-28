ca97d35a3c96807052133b1675260b2ebb2cac1a
Aleksandr_JS <aleksandr-js-php@yandex.ru>
2025-01-29 00:15:55 +0200

    add codellama:13b

--- config/constants.py
+++ config/constants.py
@@ -1,2 +1,2 @@
 # list of available models that are installed locally using ollama
-models_app = ["qwen2.5-coder:7b", "qwen2.5-coder:3b", "deepseek-r1:14b", "deepseek-r1:8b", "deepseek-coder-v2:16b", "llama3.2-vision:11b", "aya-expanse:8b", "marco-o1:7b"]
+models_app = ["codellama:13b", "qwen2.5-coder:7b", "qwen2.5-coder:3b", "deepseek-r1:14b", "deepseek-r1:8b", "deepseek-coder-v2:16b", "llama3.2-vision:11b", "aya-expanse:8b", "marco-o1:7b"]
